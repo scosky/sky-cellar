7e82a3d27e5b8fdbb7af8252c58df9060cc47fc6
sky <sky@sky.local>
2016-05-15 23:29:59 +0800

    main test method

---
+++ sky-service/src/main/java/com/sky/main/SkyMain.java
@@ -0,0 +1,12 @@
+package com.sky.main;
+
+/**
+ * Created by sky on 16/5/15.
+ */
+public class SkyMain {
+
+    public static void main(String[] args) {
+        System.out.println("hello word!");
+    }
+
+}
