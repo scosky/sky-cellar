b5effe0a72bb6796e8bf8cff42469cafbf5a7e70
scosky <shy21390@126.com>
2016-05-20 13:57:57 +0800

    modify SkyMain

--- sky-service/src/main/java/com/sky/main/SkyMain.java
+++ sky-service/src/main/java/com/sky/main/SkyMain.java
@@ -8,6 +8,8 @@ public class SkyMain {
     public static void main(String[] args) {
         System.out.println("hello word!");
 
+        String name = "这是第二个分支";
+
         System.out.println("当前项目版本是 0.1");
     }
 
