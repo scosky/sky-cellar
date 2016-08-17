a962653f95607cc47486f84993ad8154aaff43c2
scosky <shy21390@126.com>
2016-08-17 17:12:42 +0800

    jc

--- sky-service/src/main/java/com/sky/main/SkyMain.java
+++ sky-service/src/main/java/com/sky/main/SkyMain.java
@@ -7,11 +7,6 @@ public class SkyMain {
 
     public static void main(String[] args) {
         System.out.println("hello word!");
-
-        System.out.println("当前分支是sky-cellar-0.3");
-
-        String name = "这是第二个分支";
-
     }
 
 }
